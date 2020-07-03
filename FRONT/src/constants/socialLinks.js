4a864349548f1042d4f622bed020f10dd47c5231
Artur Gawron <artur@gawron.me>
2020-07-03 21:31:52 +0200

    Add links to social

--- FRONT/src/constants/socialLinks.js
+++ FRONT/src/constants/socialLinks.js
@@ -2,8 +2,7 @@ import React from 'react'
 import {
   FaFacebookSquare,
   FaLinkedin,
-  FaDribbbleSquare,
-  FaBehanceSquare,
+  FaInstagramSquare,
   FaTwitterSquare,
 } from 'react-icons/fa'
 
@@ -11,27 +10,22 @@ const data = [
   {
     id: 1,
     icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-    url: 'https://www.twitter.com',
+    url: 'https://www.facebook.com/artur.elldrigar.gawron',
   },
   {
     id: 2,
     icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-    url: 'https://www.twitter.com',
+    url: 'https://www.linkedin.com/in/artur-gawron-41bb40138/',
   },
   {
     id: 3,
-    icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
-    url: 'https://www.twitter.com',
+    icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
+    url: 'https://twitter.com/Elldrigar',
   },
   {
     id: 4,
-    icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
-    url: 'https://www.twitter.com',
-  },
-  {
-    id: 5,
-    icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
-    url: 'https://www.twitter.com',
+    icon: <FaInstagramSquare className="social-icon"></FaInstagramSquare>,
+    url: 'https://www.instagram.com/arturgawron.me/',
   },
 ]
 const links = data.map(link => {
