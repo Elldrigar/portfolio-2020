e8509089a56fbdda3e41907b0ab07fa38c488b9c
Artur Gawron <artur@gawron.me>
2020-07-07 18:37:49 +0200

    Add Skills component

--- FRONT/src/components/Skills.js
+++ FRONT/src/components/Skills.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import Title from './Title'
-import services from '../constants/services'
-const Services = () => {
-  return <h2>services component</h2>
+import skills from '../constants/skills'
+
+const Skills = () => {
+  return (
+    <section className="section bg-grey">
+      <Title title="Umiejętności" />
+      <div className="section-center skills-center">
+        {skills.map(skill => {
+          const { id, title, skills, text } = skill
+          return (
+            <article key={id} className="skill">
+              <h4>{title}</h4>
+              <div className="underline"></div>
+              <p>{skills}</p>
+              <p>{text}</p>
+            </article>
+          )
+        })}
+      </div>
+    </section>
+  )
 }
 
-export default Services
+export default Skills
