2f58397cba5acf290e13ec0315b351818bd4fb41
Artur Gawron <artur@gawron.me>
2020-07-08 21:00:01 +0200

    Add props and Title

--- FRONT/src/components/Projects.js
+++ FRONT/src/components/Projects.js
@@ -2,8 +2,12 @@ import React from 'react'
 import Title from './Title'
 import Project from './Project'
 import { Link } from 'gatsby'
-const Projects = () => {
-  return <h2>projects list</h2>
+const Projects = ({ projects, title, showLink }) => {
+  return (
+    <section className="section project">
+      <Title title={title} />
+    </section>
+  )
 }
 
 export default Projects
