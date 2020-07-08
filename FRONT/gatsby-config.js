803e7846f2be23d8e511a66bd4297461ae26fd3d
Artur Gawron <artur@gawron.me>
2020-07-08 19:55:51 +0200

    Add contentType projects

--- FRONT/gatsby-config.js
+++ FRONT/gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
       options: {
         apiURL: `http://localhost:1337`,
         queryLimit: 1000,
-          contentTypes : [`jobs`],
+          contentTypes : [`jobs`,`projects`],
 
 
 
