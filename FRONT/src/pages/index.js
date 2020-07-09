5fc388cd335277ea89b4bc93919d746e56f3f856
Artur Gawron <artur@gawron.me>
2020-07-09 23:34:42 +0200

    Update query for blog

--- FRONT/src/pages/index.js
+++ FRONT/src/pages/index.js
@@ -44,5 +44,23 @@ export const query = graphql`
         }
       }
     }
+    allStrapiBlogs(sort: { fields: date, order: DESC }, limit: 3) {
+      nodes {
+        slug
+        content
+        desc
+        date(formatString: "DD-MM, YYYYr")
+        id
+        title
+        category
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
   }
 `
