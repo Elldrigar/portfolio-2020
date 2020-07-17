05a6ee92af8cb80c5e85843d8aa4c03d1d40606f
Artur Gawron <artur@gawron.me>
2020-07-17 19:50:20 +0200

    Remove unused Link

--- FRONT/src/components/Jobs.js
+++ FRONT/src/components/Jobs.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Title from './Title'
 import { FaAngleDoubleRight } from 'react-icons/fa'
 import { graphql, useStaticQuery } from 'gatsby'
-import { Link } from 'gatsby'
 
 const query = graphql`
   {
@@ -58,8 +57,6 @@ const Jobs = () => {
             )
           })}
         </article>
-
-        
       </div>
     </section>
   )
